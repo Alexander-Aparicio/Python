7ac80f4609e13010f757005db6fedfcec0322adc
Alexander-Aparicio <alexdel2022@gmail.com>
2022-06-27 11:58:21 -0500

    uso de requests

--- semana01/dia3/pokedex/descargarpokemon.py
+++ semana01/dia3/pokedex/descargarpokemon.py
@@ -1,5 +1,8 @@
 import requests
 import json
+import webbrowser
+from PIL import Image
+
 
 BASE_URL= 'http://pokeapi.co/api/v2/pokemon/'
 
@@ -13,4 +16,20 @@ def consultarPokedApi(nombrePokemon):
       return None
 
 if __name__ == '__main__':
-  pass
+  buscar = input('ingrese el pokemon a buscar: ')
+  pokemon = consultarPokedApi(buscar)
+  if pokemon == None:
+    print('No existe Pokemon con ese nombre')
+  else:
+    # print(pokemon)
+    sprites = pokemon['sprites']
+    print(sprites['front_default'])
+    f = open(buscar + '.jpg','wb')
+    # wb: es para indicar que puede escribir coódigo binario
+    response = requests.get(sprites['front_default'])
+    f.write(response.content)
+    f.close()
+    print("se descarga la imagen del pokemon")
+    webbrowser.open(sprites['front_default'])
+    im = Image.open(buscar + '.jpg')
+    im.show()
